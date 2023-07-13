361cbd1305677027144824cc2bb901a1ed590516
Tem Pearse <tem.pearse01@gmail.com>
2023-07-13 15:37:39 +1200

    background landing vid added

--- client/pages/landingPage.tsx
+++ client/pages/landingPage.tsx
@@ -4,6 +4,9 @@ import Footer from '../components/Footer/Footer'
 function LandingPage() {
   return (
     <div>
+      <video className="w-full" autoPlay muted loop>
+        <source src="landingBackground/IMG-1401.mp4" type="video/mp4"></source>
+      </video>
       <EnterButton />
       <p>this is the landing page</p>
       <Footer />
