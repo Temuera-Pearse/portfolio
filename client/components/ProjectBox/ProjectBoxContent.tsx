782befd85625433d21f2182722ab815a4b11ade3
Tem Pearse <tem.pearse01@gmail.com>
2023-07-18 12:53:44 +1200

    box content component made

---
+++ client/components/ProjectBox/ProjectBoxContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProjectBox from './ProjectBox'
+
+function ProjectBoxContent() {
+  return (
+    <div>
+      <ProjectBox imageSrc="" description="" stacks={[]} />
+    </div>
+  )
+}
+export default ProjectBoxContent
