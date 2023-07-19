81477ff98d7f6b07ab66ec6b2b3bc4ad59512cb1
Tem Pearse <tem.pearse01@gmail.com>
2023-07-19 21:33:47 +1200

    hello

--- client/apis/fruits.ts
+++
@@ -1,9 +0,0 @@
-import request from 'superagent'
-
-const rootUrl = '/api/v1'
-
-export function getTem(): Promise<string[]> {
-  return request.get(rootUrl + '/Tem').then((res) => {
-    return res.body.fruits
-  })
-}
--- server/db/tems.ts
+++
@@ -1,6 +0,0 @@
-import connection from './connection'
-import { Tem } from '../../models/tem'
-
-export function getAllTems(db = connection): Promise<Tem[]> {
-  return db('tem').select()
-}
