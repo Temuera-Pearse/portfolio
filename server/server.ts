17107f6ad3e2c05c03fbfe649c062c3226c07d53
Tem Pearse <tem.pearse01@gmail.com>
2023-07-20 13:12:45 +1200

    this is getting tedious lol

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,9 @@
 import express from 'express'
 import { join } from 'node:path'
 
-import fruitRoutes from './routes/tems'
-
 const server = express()
 
 server.use(express.json())
 server.use(express.static(join(__dirname, 'public')))
-
-server.use('/api/v1/Tem', fruitRoutes)
 
 export default server
