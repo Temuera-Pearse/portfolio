330cab3bab13174c1cb29cca7e0784ceb0aba083
Tem Pearse <tem.pearse01@gmail.com>
2023-07-20 13:27:42 +1200

    removed server

--- server/server.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { join } from 'node:path'
-
-const server = express()
-
-server.use(express.json())
-server.use(express.static(join(__dirname, 'public')))
-
-export default server
