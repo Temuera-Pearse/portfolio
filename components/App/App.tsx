8c1c88fc16fba2c9cd9f57e2c37eaab12599f09d
Tem Pearse <tem.pearse01@gmail.com>
2023-07-20 14:53:30 +1200

    Updates

--- components/App/App.tsx
+++ components/App/App.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom'
 import { useEffect } from 'react'
-import { useAppDispatch, useAppSelector } from '../../hooks'
 import EnterButton from '../EnterButton/EnterButton'
 
 function App() {
